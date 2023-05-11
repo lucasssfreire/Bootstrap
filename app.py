c9a8b3d208653ba18724a2061ad102c1fec2cccd
LUCAS SOARES DA SILVA FREIRE <lucas.ssfreire@senacsp.edu.br>
2023-05-11 11:49:19 -0300

    Menus

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, template_rendered, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Ola mundo'
+    return render_template('index.html')
+
+@app.route("/sobreEAD")
+def ead():
+    return render_template('sobre_EAD.html') 
+
+@app.route("/cursos")
+def cursos():
+    return render_template('cursos.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+@app.route("/login")
+def login():
+    titulo = 'Login de Acesso'
+    descricao = 'Formulario de login'
+    return render_template('login.html',titulo=titulo,descricao=descricao)
+
 
 if __name__=='__main__':
     app.run(debug=True)
+
