358d98e91d99cf0019fc67303208cc00242c30bf
LUCAS SOARES DA SILVA FREIRE <lucas.ssfreire@senacsp.edu.br>
2023-05-25 12:09:18 -0300

    site

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from forms import formLogin, formNovoUsuario
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '997181c5137d5ee684f482e6274956e4861e3460653aa4c5317d7065bef83c7c'
 
 @app.route("/")
 def index():
     return render_template('index.html')
-
+ 
 @app.route("/sobreEAD")
 def ead():
     return render_template('sobre_EAD.html') 
@@ -15,14 +19,17 @@ def cursos():
     return render_template('cursos.html')
 
 @app.route("/contato")
-def contato():
+def contato():  
     return render_template('contato.html')
 
-@app.route("/login")
+@app.route("/login", methods=['get','post'])
 def login():
     titulo = 'Login de Acesso'
-    descricao = 'Formulario de login'
-    return render_template('login.html',titulo=titulo,descricao=descricao)
+    descricao = 'Formulario de Login'
+
+    form_login = formLogin()
+    form_novo_usuario = formNovoUsuario()
+    return render_template('login.html',titulo=titulo,descricao=descricao,form_login=form_login,form_novo_usuario=form_novo_usuario)
 
 
 if __name__=='__main__':
